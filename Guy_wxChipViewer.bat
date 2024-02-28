e4dd8e58499868854008f726d9b76cc376d5c7e4
Guy Soffer (GSOF) <gsoffer@yahoo.com>
2024-02-27 18:43:29 -0800

    First commit

---
+++ Guy_wxChipViewer.bat
@@ -0,0 +1,2 @@
+rem C:\GuySoftware\UdpTo_wxChipViewer\UdpTo_wxChipViewer.exe -f ArrayChip.cfg -w 380 -v 500 -u 7010
+C:\GuySoftware\UdpTo_wxChipViewer\UdpTo_wxChipViewer.exe -f ArrayChip_01.cfg -w 380 -v 500 -u 8010 -p 8001
